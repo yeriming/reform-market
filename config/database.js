0efad8e719c4f8f7f00ebdea736ae8e658a4d8ca
yeriming <100740625+yeriming@users.noreply.github.com>
2022-06-25 15:29:00 +0900

    innit commit

--- config/database.js
+++ config/database.js
@@ -1,13 +1,21 @@
+const path = require('path');
+
+
+
 module.exports = ({ env }) => ({
+
   connection: {
-    client: 'postgres',
+
+    client: 'sqlite',
+
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'reform-market'),
-      user: env('DATABASE_USERNAME', 'admin'),
-      password: env('DATABASE_PASSWORD', 'Asdf1234!'),
-      ssl: env.bool('DATABASE_SSL', true),
+
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+
     },
+
+    useNullAsDefault: true,
+
   },
+  
 });
